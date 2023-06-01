f9f79e03aab5fa9de188361118244af1f093ea7a
Latif Altay <latifaltay00@gmail.com>
2023-06-01 04:30:48 +0300

    HashMap<String,String> directory = new HashMap<String, String>();

---
+++ TelephoneDirectory/src/Directory.java
@@ -0,0 +1,6 @@
+import java.util.HashMap;
+
+public class Directory {
+    HashMap<String,String> directory = new HashMap<String, String>();
+
+}
--- TelephoneDirectory/src/Main.java
+++ TelephoneDirectory/src/Main.java
@@ -1,7 +1,27 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int selection = 1;
+        while(selection != 0){
+            System.out.println("Çıkış yapmak İçin: 0");
+            System.out.println("Rehbere Kayıt Eklemek İçin: 1");
+            System.out.println("Rehberden Kayıt Silmek İçin: 2");
+            System.out.println("Rehberi Görüntülemek İçin: 3");
+        }
+
+        HashMap<String,String> directory = new HashMap<String, String>();
+
+        System.out.print("Lütfen isim giriniz: ");
+        String name = scan.nextLine();
+        System.out.print("Lütfen Numara giriniz: ");
+        String number = scan.nextLine();
+
+        directory.put(name,number);
+
+
 
     }
 }
